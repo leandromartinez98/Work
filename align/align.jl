60d6c65cf9d38aa4c9b0dbaa7cb76be503a36390
Leandro Martinez <lmartine@unicamp.br>
2022-04-06 15:20:42 -0300

    updated align.jl

--- align/align.jl
+++ align/align.jl
@@ -1,127 +1,111 @@
 """
 
-  function align(x,y) -> xnew
+```
+align(x, y, xmass, ymass)
+```
 
-  aligns two structures [sets of points in 3D space]. Solves
-  the "Procrustes" problem. Structures are expected to be of the same size, and the 
-  correspondence is assumed from the vector indices. 
- 
-  Returns x aligned, by performing the rigid body transformation [rotation
-  and translation that minimizes the RMSD between x and y].
- 
-  x, y, and xnew (return) are matrices of dimensions (n,3) 
-  (n is the number of points, 3 is the dimension of the space).
- 
-  L. Martinez, Institute of Chemistry - University of Campinas
-  Jan 04, 2019
+or
+
+```
+align!(xnew, x, y, xmass, ymass)
+```
+
+aligns two structures (sets of points in 3D space), represented by vectors of static vectors. Solves the "Procrustes" problem. Structures are expected to be of the same size, and the correspondence is assumed from the vector indices. 
+
+The non-mutation version `align` returns a new array, corresponding to the 
+`x` coordinates rotated and translated to minimize the RMSD relative to `y`. 
+
+The mutation call `align!` updates the `xnew` array.
+
+L. Martinez, Institute of Chemistry - University of Campinas, Apr 6, 2022.
 
 """
 
+using StaticArrays
 using LinearAlgebra
 
-function align( x :: Matrix{Float64}, y :: Matrix{Float64} )
+# Non-mutating
+function align(x, y, xmass, ymass) 
+    xnew = similar(x)
+    align!(xnew, x, y, xmass, ymass)
+    return xnew
+end
 
-  n = size(x,1)
+# Mutating
+@views function align!(
+    xnew::AbstractVector{SVector{3,T}},
+    x::AbstractVector{SVector{3,T}}, 
+    y::AbstractVector{SVector{3,T}}, 
+    xmass::AbstractVector{T}, 
+    ymass::AbstractVector{T}
+) where T
 
-  # Computing centroid
-
-  cmx = zeros(3)
-  cmy = zeros(3)
-  for i in 1:n
-    for j in 1:3
-      cmx[j] = cmx[j] + x[i,j]
-      cmy[j] = cmy[j] + y[i,j]
+    # Computing centers of mass
+    cmx = zero(SVector{3,T})
+    cmy = zero(SVector{3,T})
+    tot_mass_x = zero(T)
+    tot_mass_y = zero(T)
+    for i in eachindex(x)
+        cmx += x[i] * xmass[i]
+        cmy += y[i] * ymass[i]
+        tot_mass_x += xmass[i]
+        tot_mass_y += ymass[i]
     end
-  end
-  cmx = cmx / n
-  cmy = cmy / n
+    cmx = cmx / tot_mass_x
+    cmy = cmy / tot_mass_y
 
-  # Translating both sets to the origin
-
-  for i in 1:n
-    for j in 1:3
-      x[i,j] = x[i,j] - cmx[j]
-      y[i,j] = y[i,j] - cmy[j]
+    # Translating both sets to the origin
+    for i in eachindex(x)
+        x[i] = x[i] - cmx
+        y[i] = y[i] - cmy
     end
-  end
 
-  # Computing the quaternion matrix
-
-  xm = Vector{Float64}(undef,n)
-  ym = Vector{Float64}(undef,n)
-  zm = Vector{Float64}(undef,n)
-  xp = Vector{Float64}(undef,n)
-  yp = Vector{Float64}(undef,n)
-  zp = Vector{Float64}(undef,n)
-  for i in 1:n
-    xm[i] = y[i,1] - x[i,1]
-    ym[i] = y[i,2] - x[i,2]
-    zm[i] = y[i,3] - x[i,3]
-    xp[i] = y[i,1] + x[i,1]
-    yp[i] = y[i,2] + x[i,2]
-    zp[i] = y[i,3] + x[i,3]
-  end
-
-  q = zeros(4,4)
-  for i in 1:n
-    q[1,1] = q[1,1] + xm[i]^2 + ym[i]^2 + zm[i]^2
-    q[1,2] = q[1,2] + yp[i]*zm[i] - ym[i]*zp[i]
-    q[1,3] = q[1,3] + xm[i]*zp[i] - xp[i]*zm[i]
-    q[1,4] = q[1,4] + xp[i]*ym[i] - xm[i]*yp[i]
-    q[2,2] = q[2,2] + yp[i]^2 + zp[i]^2 + xm[i]^2
-    q[2,3] = q[2,3] + xm[i]*ym[i] - xp[i]*yp[i]
-    q[2,4] = q[2,4] + xm[i]*zm[i] - xp[i]*zp[i]
-    q[3,3] = q[3,3] + xp[i]^2 + zp[i]^2 + ym[i]^2
-    q[3,4] = q[3,4] + ym[i]*zm[i] - yp[i]*zp[i]
-    q[4,4] = q[4,4] + xp[i]^2 + yp[i]^2 + zm[i]^2
-  end
-  q[2,1] = q[1,2]
-  q[3,1] = q[1,3]
-  q[3,2] = q[2,3]
-  q[4,1] = q[1,4]
-  q[4,2] = q[2,4]
-  q[4,3] = q[3,4]          
-
-  # Computing the eigenvectors 'v' of the q matrix
-
-  v = LinearAlgebra.eigvecs(q)
-
-  # Compute rotation matrix
-  
-  u = Matrix{Float64}(undef,3,3)
-  u[1,1] = v[1,1]^2 + v[2,1]^2 - v[3,1]^2 - v[4,1]^2
-  u[1,2] = 2. * ( v[2,1]*v[3,1] + v[1,1]*v[4,1] )
-  u[1,3] = 2. * ( v[2,1]*v[4,1] - v[1,1]*v[3,1] )
-  u[2,1] = 2. * ( v[2,1]*v[3,1] - v[1,1]*v[4,1] )
-  u[2,2] = v[1,1]^2 + v[3,1]^2 - v[2,1]^2 - v[4,1]^2
-  u[2,3] = 2. * ( v[3,1]*v[4,1] + v[1,1]*v[2,1] )
-  u[3,1] = 2. * ( v[2,1]*v[4,1] + v[1,1]*v[3,1] )
-  u[3,2] = 2. * ( v[3,1]*v[4,1] - v[1,1]*v[2,1] )
-  u[3,3] = v[1,1]^2 + v[4,1]^2 - v[2,1]^2 - v[3,1]^2      
-
-  # Rotate vector x [will be stored in xnew], and restore y
-
-  xnew = zeros(n,3)
-  for i in 1:n
-    for j in 1:3
-      for k in 1:3
-        xnew[i,j] = xnew[i,j] + u[j,k] * x[i,k]
-      end 
+    # Computing the quaternion matrix
+    q = zero(MMatrix{4,4,T,16})
+    for i in eachindex(x)
+        xm = y[i] - x[i]
+        xp = y[i] + x[i]
+        q[1,1] = q[1,1] + xm[1]^2 + xm[2]^2 + xm[3]^2
+        q[1,2] = q[1,2] + xp[2]*xm[3] - xm[2]*xp[3]
+        q[1,3] = q[1,3] + xm[1]*xp[3] - xp[1]*xm[3]
+        q[1,4] = q[1,4] + xp[1]*xm[2] - xm[1]*xp[2]
+        q[2,2] = q[2,2] + xp[2]^2 + xp[3]^2 + xm[1]^2
+        q[2,3] = q[2,3] + xm[1]*xm[2] - xp[1]*xp[2]
+        q[2,4] = q[2,4] + xm[1]*xm[3] - xp[1]*xp[3]
+        q[3,3] = q[3,3] + xp[1]^2 + xp[3]^2 + xm[2]^2
+        q[3,4] = q[3,4] + xm[2]*xm[3] - xp[2]*xp[3]
+        q[4,4] = q[4,4] + xp[1]^2 + xp[2]^2 + xm[3]^2
     end
-  end
+    q[2,1] = q[1,2]
+    q[3,1] = q[1,3]
+    q[3,2] = q[2,3]
+    q[4,1] = q[1,4]
+    q[4,2] = q[2,4]
+    q[4,3] = q[3,4]          
 
-  # Translate vector to the centroid of y [and restore x and y]
+    # Computing the eigenvectors 'v' of the q matrix
+    v = LinearAlgebra.eigvecs(q)[:,1]
 
-  for i in 1:n
-    for j in 1:3
-      xnew[i,j] = xnew[i,j] + cmy[j]
-      y[i,j] = y[i,j] + cmy[j]
-      x[i,j] = x[i,j] + cmx[j]
+    # Compute rotation matrix
+    u = zero(MMatrix{3,3,T,9})
+    u[1,1] = v[1]^2 + v[2]^2 - v[3]^2 - v[4]^2
+    u[1,2] = 2 * ( v[2]*v[3] + v[1]*v[4] )
+    u[1,3] = 2 * ( v[2]*v[4] - v[1]*v[3] )
+    u[2,1] = 2 * ( v[2]*v[3] - v[1]*v[4] )
+    u[2,2] = v[1]^2 + v[3]^2 - v[2]^2 - v[4]^2
+    u[2,3] = 2 * ( v[3]*v[4] + v[1]*v[2] )
+    u[3,1] = 2 * ( v[2]*v[4] + v[1]*v[3] )
+    u[3,2] = 2 * ( v[3]*v[4] - v[1]*v[2] )
+    u[3,3] = v[1]^2 + v[4]^2 - v[2]^2 - v[3]^2      
+
+    # Rotate vector x, will be stored in xnew, and restore input arrays
+    for i in eachindex(x)
+        xnew[i] = cmy + u * x[i]
+        x[i] = x[i] + cmx
+        y[i] = y[i] + cmy
     end
-  end
 
-  return xnew
-
+    return nothing
 end
 
 
--- align/example.jl
+++ align/example.jl
@@ -1,27 +1,23 @@
 
+using Statistics: mean
+using LinearAlgebra: norm_sqr
 using DelimitedFiles
 include("./align.jl")
 
-x = readdlm("./1.xyz")
-y = readdlm("./2.xyz")
+x_read = readdlm("./1.xyz")
+y_read = readdlm("./2.xyz")
+x = [ SVector{3,Float64}(row) for row in eachrow(x_read) ]
+y = [ SVector{3,Float64}(row) for row in eachrow(y_read) ]
+xmass = [ 1. for _ in 1:length(x) ]
+ymass = [ 1. for _ in 1:length(y) ]
 
 # Align x to y (same size, correspondence given by sequence)
+z = align(x, y, xmass, ymass)
 
-z = align(x,y)
+# or
+# xnew = similar(x)
+# align!(xnew, x, y, xmass, ymass)
 
 # Compute rmsd before and after alignment
-
-rmsd_before = 0.
-rmsd_after = 0.
-n = size(x,1)
-for i in size(x,1)
-  global rmsd_before, rmsd_after, n
-  rmsd_before = rmsd_before + (x[i,1] - y[i,1])^2 + (x[i,2] - y[i,2])^2 + (x[i,3] - y[i,3])^2
-  rmsd_after = rmsd_after + (z[i,1] - y[i,1])^2 + (z[i,2] - y[i,2])^2 + (z[i,3] - y[i,3])^2
-end
-rmsd_before = sqrt(rmsd_before/n)
-rmsd_after = sqrt(rmsd_after/n)
-println("rmsd before = ", rmsd_before)
-println("rmsd after = ", rmsd_after)
-
-
+println("rmsd before = ", sqrt(mean(norm_sqr, x - y))) 
+println("rmsd after = ", sqrt(mean(norm_sqr, z - y))) 
